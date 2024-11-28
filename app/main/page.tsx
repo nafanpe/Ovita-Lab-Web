28ea62654c9302ef2afc6b8bf20c67bf5b1af1c1
mrsajadpp <mrsajadpp@gmail.com>
2024-11-28 19:54:54 +0530

    bento ui addded

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,24 +1,38 @@
-import '@/public/css/not-found.css';
+import '@/public/css/index.css';
 import Header from '@/components/stable_header';
 
 
 export const metadata = {
-  title: "Innovating AI, Robotics, and Software Solutions for a Smarter Tomorrow",
-  description: "Ovita Lab is a startup dedicated to developing artificial intelligence, robotics, and software solutions to address real-world challenges.",
-  openGraph: {
-    images: ['/img/opengraph/image.png'],
-  }
+    title: "Innovating AI, Robotics, and Software Solutions for a Smarter Tomorrow",
+    description: "Ovita Lab is a startup dedicated to developing artificial intelligence, robotics, and software solutions to address real-world challenges.",
+    openGraph: {
+        images: ['/img/opengraph/image.png'],
+    }
 };
 
 export default function Custom404() {
-  return (
-    <>
-      <Header />
-      <div className="notfound-container">
-      <img src="/img/icons/graphics/error.png" alt="Not Found Image" className="notfound-image" />
-        <h1 className="notfound-title">We Are Undergoing Maintenance</h1>
-        <p className="notfound-message">Our website is currently being updated. Please check back soon!</p>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div className="bento">
+                <div className="grid">
+                    <div className="item">
+                        <img src="https://cdn.pixabay.com/photo/2024/07/15/17/21/ai-generated-8897456_1280.jpg" alt="Placeholder 5" />
+                    </div>
+                    <div className="item">
+                        <img src="https://cdn.pixabay.com/photo/2024/01/10/16/13/drone-8499903_1280.jpg" alt="Placeholder 2" />
+                    </div>
+                    <div className="item">
+                        <img src="https://cdn.pixabay.com/photo/2024/01/10/16/22/woman-8499959_1280.jpg" alt="Placeholder 1" />
+                    </div>
+                    <div className="item">
+                        <img src="https://cdn.pixabay.com/photo/2020/03/17/17/46/engineer-4941342_1280.jpg" alt="Placeholder 3" />
+                    </div>
+                    <div className="item">
+                        <img src="https://cdn.pixabay.com/photo/2023/08/09/18/02/man-8180076_1280.jpg" alt="Placeholder 4" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
     images: {
         domains: ['via.placeholder.com', "images.pexels.com", "media.licdn.com", "dqy38fnwh4fqs.cloudfront.net", 'picsum.photos', 'bs-uploads.toptal.io', 'datawider.com', 'www.felixvemmer.com'],
-        
     }
 };
 
